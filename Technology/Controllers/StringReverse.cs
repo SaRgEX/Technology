6d1c963d096b10a79d0e21a26c2b73f36c9a21aa
SaRgEX <maze_98@mail.ru>
2024-07-09 18:36:16 +0500

    task1

---
+++ Technology/Controllers/StringReverse.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Technology.Service;
+
+namespace Technology.Controllers
+{
+    [ApiController]
+    public class StringReverse
+    {
+        [HttpPost("/reverse")]
+        public string Reverse(string input)
+        {
+            return StringReverses.ReverseByLenght(input);
+        }
+    }
+}
