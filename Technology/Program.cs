9458aadf61c90b1afcd86b7b1f9717f2406fa366
SaRgEX <maze_98@mail.ru>
2024-07-14 01:19:38 +0500

    task7

--- Technology/Program.cs
+++ Technology/Program.cs
@@ -1,8 +1,22 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() 
+    { 
+        Title = "Technology", 
+        Version = "v1",
+        Description = "Intenship Tasks",
+
+    
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
+});
 
 var app = builder.Build();
 
