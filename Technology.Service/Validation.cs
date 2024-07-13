e7f9e45a8f77b76bdb446376b7811de55340ee3d
SaRgEX <maze_98@mail.ru>
2024-07-13 16:20:56 +0500

    validation of input string
    Description: validation by using only in lowercase and latin letters

---
+++ Technology.Service/Validation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technology.Service
+{
+    public static class Validation
+    {
+        public static IEnumerable<char> WrongLetters (string input)
+        {
+            foreach (char letter in input)
+            {
+                if (letter < 'a' || letter > 'z')
+                    yield return letter;
+            }
+        }
+    }
+}
