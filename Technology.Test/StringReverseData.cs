52bfcb0a5dd07f0c4b1cb4114eebf09042cccb06
SaRgEX <maze_98@mail.ru>
2024-07-14 22:09:41 +0500

    task10

---
+++ Technology.Test/StringReverseData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Technology.Service.Sorts;
+using Technology.Service.Sorts.Tree;
+
+namespace Technology.Test
+{
+    public class StringReverseData
+    {
+        public static TestCaseData[] Counter { get; set; } =
+        {
+        new TestCaseData("abfe", new Dictionary<string, int>
+        {
+            { "a", 1 },
+            { "b", 1 },
+            { "f", 1 },
+            { "e", 1 }
+        }),
+        new TestCaseData("abbce", new Dictionary<string, int>
+        {
+            { "a", 1 },
+            { "b", 2 },
+            { "c", 1 },
+            { "e", 1 }
+        })
+        };
+        public static TestCaseData[] Sorts { get; set; } =
+        {
+        new TestCaseData("bfeada", QuickSort.Sort, "aabdef").SetName("Sorting a string using QuickSort"),
+        new TestCaseData("sjhfsa", TreeSort.Sort, "afhjss").SetName("Sorting a string using TreeSort")
+        };
+    }
+}
---
+++ Technology.Test/StringReverseTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Technology.Service;
+
+namespace Technology.Test
+{
+    public class StringReverseTest
+    {
+        [TestCase("a", "aa")]
+        [TestCase("abcdef", "cbafed")]
+        [TestCase("abcde", "edcbaabcde")]
+        [TestCase("", "")]
+        public void ReverseStringTest(string input, string expectedOutput)
+        {
+            var result = StringReverses.ReverseByLenght(input);
+
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+        [TestCase("abc", new char[] {})]
+        [TestCase("aBc", new char[] { 'B' })]
+        [TestCase("Abc1", new char[] { 'A', '1' })]
+        public void WrongLetter(string input, IEnumerable<char> expectedOutput)
+        {
+            var result = Validation.WrongLetters(input);
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+
+        [TestCaseSource(typeof(StringReverseData), nameof(StringReverseData.Counter))]
+        public void LettersCount(string input, Dictionary<string, int> expectedOutput)
+        {
+            var result = StringReverses.CountLetters(input);
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+        [TestCase("abc", "")]
+        [TestCase("a", "")]
+        [TestCase("abcde", "abcde")]
+        [TestCase("defaafed", "efaafe")]
+        [TestCase("abadb", "aba")]
+        public void LongestSubstring(string input, string expectedOutput)
+        {
+            var result = StringReverses.LongestSubstring(input);
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+        [TestCase("asdb", "quick", "abds")]
+        [TestCase("qwerty", "tree", "eqrtwy")]
+        public void Sorts(string input, string type, string expectedOutput)
+        {
+            var result = StringReverses.Sort(input, type);
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+    }
+}
